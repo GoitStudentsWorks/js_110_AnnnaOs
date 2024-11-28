bfebbb505577c3f073e1e5c4c0681eee25222c6e
AnnnaOs <148139845+AnnnaOs@users.noreply.github.com>
2024-11-28 19:19:31 +0200

    Revert "fix"

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,6 +1,5 @@
 const menuLink = document.querySelectorAll('.header-mobile-navigation a');
 const menuContainer = document.querySelector('.js-menu-container');
-const body=document.body;
 
 document.addEventListener('DOMContentLoaded', () => {
 const openMenuButton = document.querySelector('.js-open-menu');
@@ -11,12 +10,13 @@ if (!menuContainer || !openMenuButton || !closeMenuButton) {
 }
 
 // Добавляем класс 'dark-theme' к body после загрузки DOM
+// document.body.classList.add('dark-theme');
+// console.log("Класс 'dark-theme' добавлен после загрузки DOM.");
 
 // Открытие меню
 openMenuButton.addEventListener('click', () => {
   menuContainer.classList.add('active');
-  
-  body.classList.add('no-scroll');
+  // document.body.classList.toggle('no-scroll');
 
   // console.log('Меню открыто');
 });
@@ -24,8 +24,6 @@ openMenuButton.addEventListener('click', () => {
 // Закрытие меню
 closeMenuButton.addEventListener('click', () => {
   menuContainer.classList.remove('active');
-  body.classList.remove('no-scroll');
-
   // console.log('Меню закрыто');
 });
 
@@ -33,8 +31,6 @@ closeMenuButton.addEventListener('click', () => {
 menuContainer.addEventListener('click', event => {
   if (event.target === menuContainer) {
     menuContainer.classList.remove('active');
-    body.classList.remove('no-scroll');
-
     // console.log('Клик вне меню, меню закрыто');
   }
 });
@@ -43,19 +39,17 @@ menuContainer.addEventListener('click', event => {
 menuLink.forEach(link => {
   
   link.addEventListener('click', e => {
-          body.classList.remove('no-scroll');
-
     e.preventDefault(); // Забороняємо стандартну поведінку браузера
     const targetId = link.getAttribute('href').substring(1); // Отримуємо ID цільового елемента
     const targetElement = document.getElementById(targetId);
 
     if (targetElement) {
-      
       // Плавний скрол до секції
       targetElement.scrollIntoView({ behavior: 'smooth' });
 
       // Закриваємо меню після переходу
       menuContainer.classList.remove('active');
+      document.body.classList.remove('no-scroll');
     }
   });
 });
