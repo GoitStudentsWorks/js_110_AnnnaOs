f4f38a4bfab77a5c51197422f1250c6a82584e31
AnnnaOs <preciousanutka@gmail.com>
2024-11-28 19:01:38 +0200

    fix fix

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,44 +1,51 @@
 const menuLink = document.querySelectorAll('.header-mobile-navigation a');
 const menuContainer = document.querySelector('.js-menu-container');
+const body = document.body;
 
 document.addEventListener('DOMContentLoaded', () => {
-const openMenuButton = document.querySelector('.js-open-menu');
-const closeMenuButton = document.querySelector('.js-close-menu');
-if (!menuContainer || !openMenuButton || !closeMenuButton) {
-  // console.error('Элементы управления меню не найдены!');
-  return;
-}
-
-// Добавляем класс 'dark-theme' к body после загрузки DOM
-// document.body.classList.add('dark-theme');
-// console.log("Класс 'dark-theme' добавлен после загрузки DOM.");
-
-// Открытие меню
-openMenuButton.addEventListener('click', () => {
-  menuContainer.classList.add('active');
-  // document.body.classList.toggle('no-scroll');
-
-  // console.log('Меню открыто');
-});
-
-// Закрытие меню
-closeMenuButton.addEventListener('click', () => {
-  menuContainer.classList.remove('active');
-  // console.log('Меню закрыто');
-});
-
-// Закрытие меню при клике вне области меню
-menuContainer.addEventListener('click', event => {
-  if (event.target === menuContainer) {
-    menuContainer.classList.remove('active');
-    // console.log('Клик вне меню, меню закрыто');
+  const openMenuButton = document.querySelector('.js-open-menu');
+  const closeMenuButton = document.querySelector('.js-close-menu');
+  if (!menuContainer || !openMenuButton || !closeMenuButton) {
+    // console.error('Элементы управления меню не найдены!');
+    return;
   }
-});
 
+  // Добавляем класс 'dark-theme' к body после загрузки DOM
+  // document.body.classList.add('dark-theme');
+  // console.log("Класс 'dark-theme' добавлен после загрузки DOM.");
+
+  // Открытие меню
+  openMenuButton.addEventListener('click', () => {
+    menuContainer.classList.add('active');
+    // document.body.classList.toggle('no-scroll');
+
+    body.classList.add('no-scroll');
+
+    // console.log('Меню открыто');
+  });
+
+  // Закрытие меню
+  closeMenuButton.addEventListener('click', () => {
+    menuContainer.classList.remove('active');
+    body.classList.remove('no-scroll');
+
+    // console.log('Меню закрыто');
+  });
+
+  // Закрытие меню при клике вне области меню
+  menuContainer.addEventListener('click', event => {
+    if (event.target === menuContainer) {
+      menuContainer.classList.remove('active');
+      body.classList.remove('no-scroll');
+
+      // console.log('Клик вне меню, меню закрыто');
+    }
+  });
 });
 menuLink.forEach(link => {
-  
   link.addEventListener('click', e => {
+    body.classList.remove('no-scroll');
+
     e.preventDefault(); // Забороняємо стандартну поведінку браузера
     const targetId = link.getAttribute('href').substring(1); // Отримуємо ID цільового елемента
     const targetElement = document.getElementById(targetId);
--- src/js/scroll-up-btn.js
+++ src/js/scroll-up-btn.js
@@ -1,22 +1,16 @@
 const scrollUpBtn = document.querySelector('.scroll-up-btn');
-const scrollDownBtn = document.querySelector('.scroll-down-btn');
 let lastScrollTop = 0; //Запамʼятовуємо останню позицію прокрутки
 let isScrollingUp = true; //Перевірка напрямку прокрутки
-
 //Обробник позиції прокрутки
 window.addEventListener('scroll', () => {
   let currentTop = window.scrollY;
-
   //Показуємо кнопки, тільки якщо сторінка прокручена
   if (currentTop > 0) {
     //Сторінка прокручена, кнопки повинні бути видимі
     scrollUpBtn.classList.add('is-active-scroll');
-    scrollDownBtn.classList.add('is-active-scroll');
   } else {
     scrollUpBtn.classList.remove('is-active-scroll');
-    scrollDownBtn.classList.remove('is-active-scroll');
   }
-
   //Логіка приховання/відображення кнопки "Вгору"
   if (currentTop < lastScrollTop && isScrollingUp) {
     scrollUpBtn.classList.add('is-active-scroll');
@@ -25,12 +19,6 @@ window.addEventListener('scroll', () => {
     scrollUpBtn.classList.remove('is-active-scroll');
     isScrollingUp = true;
   }
-
-  //Логіка приховання кнопки "Вниз" з нижньої частини сторінки
-  if (currentTop + window.innerHeight >= document.body.scrollHeight) {
-    scrollDownBtn.classList.remove('is-active-scroll'); //Приховуємо кнопку, якщо ми в самому низу
-  }
-
   //Логіка приховання кнопки "Вверх" з нижньої та верхньої частин сторінки
   if (
     currentTop === 0 ||
@@ -38,10 +26,8 @@ window.addEventListener('scroll', () => {
   ) {
     scrollUpBtn.classList.remove('is-active-scroll'); //Приховуємо кнопку, якщо ми в самому низу або вгорі
   }
-
   lastScrollTop = currentTop <= 0 ? 0 : currentTop;
 });
-
 //Скрол вгору при кліку на кнопку
 scrollUpBtn.addEventListener('click', () => {
   window.scrollTo({
@@ -49,13 +35,4 @@ scrollUpBtn.addEventListener('click', () => {
     behavior: 'smooth',
   });
   scrollUpBtn.classList.remove('is-active-scroll');
-});
-
-//Скрол вниз при кліку на кнопку
-scrollDownBtn.addEventListener('click', () => {
-  window.scrollTo({
-    top: document.body.scrollHeight,
-    behavior: 'smooth',
-  });
-  scrollDownBtn.classList.remove('is-active-scroll');
 });
